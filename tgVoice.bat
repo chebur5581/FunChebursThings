c3be0d3fede30c9abbef1feabcb6452f34101366
chebur5581 <110908198+chebur5581@users.noreply.github.com>
2025-07-21 09:59:04 +0300

    Add files via upload
    converting any audio to telegram voice message

---
+++ tgVoice.bat
@@ -0,0 +1,15 @@
+@echo off
+set dialog="about:<input type=file id=FILE><script>FILE.click();new ActiveXObject
+set dialog=%dialog%('Scripting.FileSystemObject').GetStandardStream(1).WriteLine(FILE.value);
+set dialog=%dialog%close();resizeTo(0,0);</script>"
+
+for /f "tokens=* delims=" %%p in ('mshta.exe %dialog%') do set "file=%%p"
+echo selected  file is : "%file%"
+
+set ext=.ogg
+
+for %%f in ("%file%") do set fileout=%%~dpnf%ext%
+
+ffmpeg -i %file% -c:a libopus %fileout%
+
+explorer /select,%fileout%
